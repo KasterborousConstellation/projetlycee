312e903ed1bd1d40dd6ebd8f798d19f03ba70dc5
BuildTools <unconfigured@null.spigotmc.org>
2023-04-03 12:54:57 +0200

    ok 2

--- login/utility.py
+++ login/utility.py
@@ -1,5 +1,6 @@
 import hashlib
 from .models import Student
+from random import randint
 def sha256(mdp):
     m = hashlib.sha256()
     m.update(mdp)
@@ -13,3 +14,9 @@ def has_student(identifiant):
 def get_student(id):
     query = Student.objects.filter(identifiant=id)
     return query[0]
+def createUUID():
+    data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789"
+    char = ''
+    for i in range(20):
+        char = char + data[randint(0,len(data)-1)]
+    return char
--- login/views.py
+++ login/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_protect
-from .utility import sha256, has_student, get_student
-
+from .utility import sha256, has_student, get_student, createUUID
+from .models import Token
+import datetime
 @csrf_protect
 def loginPage(request):
     template = loader.get_template('login/index.html')
@@ -25,7 +26,9 @@ def login(request):
             #Verification que l'élève a entrer le bon mot de passe
             if(password==student.password):
                 #TODO Create token
-                return redirect(f"/soutien/?identifiant={ 'test' }")
+                token = Token(UUID=createUUID(), student=student)
+                token.save()
+                return redirect(f"/soutien/?identifiant={ token.UUID }")
             else: 
                 return redirect(loginPage)
         else:
@@ -36,6 +39,13 @@ def login(request):
     
 def site(request):
     query = request.GET.get('identifiant',None)
-    if(query==None):
+    #Recherche des tokens avec cet identifiant (Normalement y'en a qu'un)
+    a = Token.objects.filter(UUID=query)
+    #Validation du Token 
+    #TODO Expiration des Tokens
+    if(len(a)==0):
         return redirect(loginPage)
-    return HttpResponse(f"<h1>{str(query)}</h1>")
+    student = a[0].student
+    distance = ((datetime.datetime.now(datetime.timezone.utc)-a[0].creation_date))
+    print(distance)
+    return render(request,'login/login.html',{"nom":student.nom,"prenom":student.prenom,"serie":student.serie})
