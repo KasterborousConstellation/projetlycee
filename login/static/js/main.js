5952a0aaa6a789822f2dba4c1d6dbf2edbf8773c
Kasterbourous <paul.andrieu28@gmail.com>
2023-04-11 19:29:48 +0200

    11/04 modified

---
+++ login/static/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    if(!!window.chrome){
+        var buttons = document.getElementsByClassName("headcol");
+        for(var i=0; i<buttons.length;++i){
+            buttons[i].classList.add("chromefix");
+        }
+        var buttons = document.getElementsByClassName("event");
+        for(var i=0; i<buttons.length;++i){
+            buttons[i].classList.add("chromefix-2");
+        }
+    }
+
+}
