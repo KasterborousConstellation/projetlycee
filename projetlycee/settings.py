c07b3606bc435c9da452d43fe26ea7dd3d0098ed
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 20:28:36 +0200

    commit 12/04

--- projetlycee/settings.py
+++ projetlycee/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'projetlycee.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projet',
+        'NAME': 'lycee',
         "USER": 'postgres',
         "PASSWORD": "lycee",
         "HOST": "",
